71ebe6f3789c5235596af0b0482a3c657d2ceb68
Javier García-Galera Rodríguez <galera221412@gmail.com>
2020-11-10 17:34:16 +0100

    segundo ejercicio

---
+++ JaviGarciaGaleraPracticaAngular8Ej02/src/app/Components/lista-correos/lista-correos.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-correos',
+  templateUrl: './lista-correos.component.html',
+  styleUrls: ['./lista-correos.component.scss']
+})
+export class ListaCorreosComponent implements OnInit {
+
+  correos: any[];
+
+  constructor() {
+    const correo1 = {
+      titulo: "Titulo del correo 1",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email`,
+      emisor: 'correoEmisor1@gmail.inv',
+      destinatario: 'correoReceptor@gmail.inv',
+      leido: true,
+    };
+    const correo2 = {
+      titulo: "Titulo del correo 2",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor2@gmail.inv',
+      destinatario: 'correoReceptor@gmail.inv',
+      leido: false,
+    };
+    this.correos = [];
+    this.correos.push(correo1);
+    this.correos.push(correo2);
+  }
+
+  ngOnInit() {
+  }
+
+}
